258f2536e7b1cf84c2f99900e67e23c8be0b91db
Jai Chaprana <jaychaprana@gmail.com>
2025-12-27 21:31:18 +0530

    Added Puter auths and routers using Puter API andocumentation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,12 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { useLocation, useNavigate } from "react-router";
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Renal" },
@@ -9,14 +15,29 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth } = usePuterStore();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+      navigate("/auth?next=/");
+    }
+  }, [auth.isAuthenticated]);
+
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
-        <div className="page-heading">
+        <div className="page-heading py-16">
           <h1>Track your Applications & Resume Ratings</h1>
           <h2>Review your submissions and check AI-powered feedback.</h2>
         </div>
+        {resumes.length > 0 && (
+          <div className="resumes-section">
+            {resumes.map((resume: Resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+            ))}
+          </div>
+        )}
       </section>
     </main>
   );
